fb6f02072cea708c97c2a85c228d2a0f24d207cb
rajibspal <rajibshekhar1@gmail.com>
2023-10-10 22:27:55 +0530

    Follower maneuver start after leader convergence

--- fwat_missile_salvo_guidance.m
+++ fwat_missile_salvo_guidance.m
@@ -27,10 +27,10 @@ H = L + B;
 eig_H = eig(H);
 eta_m = 1.0/(eig_H(1));
 
-eta_l = 0;
+eta_l = 3;
 eta_f = eta_m*2;
 
-tf = 10;
+tf = 25;
 
 n = numnodes(G)+1;
 r0 = 5000*ones(n,1);
@@ -54,7 +54,7 @@ y0 = -r0.*sin(theta0);
 vx0 = vm.*cos(gamma0);
 vy0 = vm.*sin(gamma0);
 
-tspan = 0:0.01:35;
+tspan = 0:0.01:30;
 s0 = [x0;y0;gamma0];
 
 % derv_func(0,s0);
@@ -77,7 +77,7 @@ plot(G,'LineWidth',2);
 i_plot = i_plot + 1;
 h(i_plot) = figure(i_plot); h(i_plot).Name = 'xy';
 plot(x,y,'LineWidth',2)
-xlim([-5000,5000]),ylim([-5000,5000])
+% xlim([-5000,5000]),ylim([-5000,5000])
 hold on
 figure(i_plot), scatter(x0,y0,'o','filled','MarkerFaceColor','r')
 hold on
@@ -124,15 +124,15 @@ grid on
 i_plot = i_plot + 1;
 h(i_plot) = figure(i_plot); h(i_plot).Name = 'am';
 plot(t,am,'LineWidth',2)
-xlabel('Time (s)'), ylabel('Lateral acceleration command (m/s^2)')
-ylim([-am_max-10,am_max+10])
+xlabel('Time (s)'), ylabel('Lateral acceleration command ($m/s^2$)')
+ylim([-100,am_max+10])
 legend('Leader','Follower 1','Follower 2','Follower 3','Follower 4')
 grid on
 
 i_plot = i_plot + 1;
 h(i_plot) = figure(i_plot); h(i_plot).Name = 'error_f';
 plot(t,error_f,'LineWidth',2,'Color','k')
-xlabel('Time (s)'), ylabel('Norm of error in time-to-go (s)')
+xlabel('Time (s)'), ylabel('Norm of time-to-go error of followers (s)')
 % ylim([-am_max-10,am_max+10])
 % legend('1','2','3','4','5')
 grid on
@@ -200,28 +200,37 @@ tgo = (r./vm).*(1 + ((thetaM.^2)/(2*K)));
 tgo_l = tgo(1);
 tgo_f = tgo(2:n);
 
+reg_factor = 0.1;
+
+Td = 30;
+tf_l = 5;
+
+e_l = (tgo_l - (Td-t));
+e_l = e_l*reg_factor;
+if (t < tf_l-1e-6)
+    u_l = -(eta_l/(tf_l-t))*(1-exp(-e_l));
+else 
+    u_l = 0;
+end
+
+
 tgod = tgo_l*ones(length(tgo_f),1);
 e_f = (tgo_f - tgod);
 
 error_f = norm(e_f);
 
-eta_l = 2;
-Td = 30;
-e_l = tgo_l - (Td-t);
-if (t < tf-1e-6)
-    u_l = -(eta_l/(tf-t))*(1-exp(-e_l));
-else 
-    u_l = 0;
-end
+e_f = e_f*reg_factor;
 
 one_n_f = ones(length(e_f),1);
-if (t < tf-1e-6)
+if (t < tf_l)
+    u_f = zeros(size(e_f));
+elseif (t >= tf_l && t < tf-1e-6)
     u_f = -(eta_f/(tf-t))*(one_n_f - exp(-H*e_f));
 else 
     u_f = zeros(size(e_f));
 end
 
-u = [u_l;u_f];
+u = [u_l/reg_factor;u_f/reg_factor];
 
 am = zeros(n,1);
 for i=1:n
